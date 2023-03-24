7fb98550b4a459fe2e9d6bfa2773a8e7904a06c1
Eliot Walt <elwalt@eu-login-33.euler.ethz.ch>
2023-03-24 10:39:36 +0100

    local slurm debugging

--- run/create_dataset.sh
+++ run/create_dataset.sh
@@ -61,63 +61,71 @@ echo "Num projects per job : $NUM_PROJECTS_PER_JOB"
 # **************************************************** LOGIC ****************************************************
 # *(1)* Configure 
 if [[ $MACHINE == "--euler" ]]; then
+  echo "Setting up env"
+  PYVERSION=3.7.4
+  GDALVESION=3.4.3
+  module load python/$PYVERSION gdal/$GDALVERSION
+  pip install gdal==`gdal-config --version` --user # do not put it in requirements, the version must match local install!
+  pip install -r requirements.txt --user
   retvalue=($(python /cluster/work/igp_psr/elwalt/pdm/rs-uncertainty/src/scripts/create_dataset.py --configure ${ARGS[@]:1}))
 else 
   retvalue=($(python /scratch/ewalt/pdm/rs-uncertainty/src/scripts/create_dataset.py --configure ${ARGS[@]:1}))
 fi
 CFG=${retvalue[0]}
 sub_cfgs=(${retvalue[@]:1})
-echo "Saved sub config files: $(dirname $CFG)"
+echo "Saved main config file : $CFG"
+echo "Saved sub config files : $(dirname $CFG)"
+echo "${sub_cfgs[@]}"
 
-# *(2)* Submit preprocessing jobs
-if [[ $MACHINE == "--euler" ]];
-then
-  echo "Submitting preprocessing job array ..."
-  retvalue=($(sbatch /cluster/work/igp_psr/elwalt/pdm/rs-uncertainty/run/slurm/preprocess_projects.sh ${sub_cfgs[@]}))
-  pp_job_array_id=${retvalue[-1]}
-  echo "Done."
-else
-  pp_pids=()
-  echo "Launching preprocessing processes ..."
-  for (( i=0; i<${#sub_cfgs[@]}; i++ ));
-  do
-    echo "Launching ${sub_cfgs[$i]}"
-    python /scratch/ewalt/pdm/rs-uncertainty/src/scripts/create_dataset.py --preprocess --cfg ${sub_cfgs[$i]} 1> dataset.$i.log 2> dataset.$i.log &
-    pids[$i]=$!
-  done
-  echo "Done. Check logs: dataset.[0-$(($i-1))].log"
-  echo "waiting on ${pids[@]} ..."
-  for pid in "${pids[@]}";
-  do
-    wait $pid
-  done
-  echo "Done."
-fi
+# # *(2)* Submit preprocessing jobs
+# if [[ $MACHINE == "--euler" ]];
+# then
+#   echo "Submitting preprocessing job array ..."
+#   retvalue=($(sbatch /cluster/work/igp_psr/elwalt/pdm/rs-uncertainty/run/slurm/preprocess_projects.sh ${sub_cfgs[@]}))
+#   pp_job_array_id=${retvalue[-1]}
+#   echo "Done."
+# else
+#   pp_pids=()
+#   echo "Launching preprocessing processes ..."
+#   for (( i=0; i<${#sub_cfgs[@]}; i++ ));
+#   do
+#     echo "Launching ${sub_cfgs[$i]}"
+#     python /scratch/ewalt/pdm/rs-uncertainty/src/scripts/create_dataset.py --preprocess --cfg ${sub_cfgs[$i]} 1> dataset.$i.log 2> dataset.$i.log &
+#     pids[$i]=$!
+#   done
+#   echo "Done. Check logs: dataset.[0-$(($i-1))].log"
+#   echo "waiting on ${pids[@]} ..."
+#   for pid in "${pids[@]}";
+#   do
+#     wait $pid
+#   done
+#   echo "Done."
+# fi
 
-# *(3)* Submit aggregation job
-if [[ $MACHINE == "--euler" ]];
-then
-  echo "Submitting aggregation job ..."
-  retvalue=($(sbatch /cluster/work/igp_psr/elwalt/pdm/rs-uncertainty/run/slurm/aggregate_projects.sh $CFG $pp_job_array_id))
-  agg_job_id=${retvalue[-1]}
-  echo "Done."
-else
-  echo "Launching aggregation process ..."
-  python /scratch/ewalt/pdm/rs-uncertainty/src/scripts/create_dataset.py --aggregate --cfg $CFG
-  pid=$!
-  wait $pid
-  echo "Done."
-  echo "Waiting on $pid ..."
-  echo "Done."
-fi
+# # *(3)* Submit aggregation job
+# if [[ $MACHINE == "--euler" ]];
+# then
+#   echo "Submitting aggregation job ..."
+#   retvalue=($(sbatch /cluster/work/igp_psr/elwalt/pdm/rs-uncertainty/run/slurm/aggregate_projects.sh $CFG $pp_job_array_id))
+#   agg_job_id=${retvalue[-1]}
+#   echo "Done."
+# else
+#   echo "Launching aggregation process ..."
+#   python /scratch/ewalt/pdm/rs-uncertainty/src/scripts/create_dataset.py --aggregate --cfg $CFG
+#   pid=$!
+#   wait $pid
+#   echo "Done."
+#   echo "Waiting on $pid ..."
+#   echo "Done."
+# fi
 
-# *(5)* Feedback
-if [[ $MACHINE == "--euler" ]];
-then
-  echo "Job ids:"
-  echo "- preprocessing job array : $pp_job_array_id"
-  echo "- aggregation job array   : $agg_job_id"
-fi
+# # *(5)* Feedback
+# if [[ $MACHINE == "--euler" ]];
+# then
+#   echo "Job ids:"
+#   echo "- preprocessing job array : $pp_job_array_id"
+#   echo "- aggregation job array   : $agg_job_id"
+# fi
 
 # *(6)* kill jobs
 # if [[ $MACHINE == "--euler" ]];
--- run/slurm/preprocess_projects.sh
+++ run/slurm/preprocess_projects.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
 
+echo "Starting preprocessing job array"
+
 # ************************************************** ARGUMENTS ***************************************************
 CONFIG_FILES=("$@")
 
+# ************************************************* ENVIRONMENT **************************************************
+echo "Setting up env"
+PYVERSION=3.7.4
+GDALVESION=3.4.3
+module load python/$PYVERSION gdal/$GDALVERSION
+pip install gdal==`gdal-config --version` --user # do not put it in requirements, the version must match local install!
+pip install -r requirements.txt --user
+
 # **************************************************** GLOBAL ****************************************************
+echo "Setting global variables"
 NUM_JOBS=${#CONFIG_FILES[@]}
-TIMESTAMP=$(basename $(dirname ${CONFIG_FILES[0]}))
+TIMESTAMP=$(basename $(dirname $(dirname ${CONFIG_FILES[0]})))
 TIME=$(python -c "from datetime import timedelta; dt=timedelta(hours=12); print(dt/$NUM_JOBS)")
 LOG_DIR=/cluster/work/igp_psr/elwalt/logs/preprocess_projects/$TIMESTAMP
 ID_SUFFIX=$SLURM_ARRAY_TASK_ID-$SLURM_ARRAY_TASK_COUNT
 JOB_NAME=preprocess_projects_$ID_SUFFIX
 LOG_FILE=$LOG_DIR/$ID_SUFFIX.log
-PYVERSION=3.7.4
-GDALVESION=3.4.3
+echo "Job name       : $JOB_NAME"
+echo "number of jobs : $NUM_JOBS"
+echo "Log file       : $LOG_FILE"
+echo "Compute time   : $TIME"
+# create log dir/file
+mkdir -p $(dirname $LOG_FILE)
+touch $LOG_FILE
 
 # **************************************************** SLURM *****************************************************
-# #SBATCH -n 1
-# #SBATCH --time=$TIME
-# #SBATCH --mem-per-cpu=6000
-# #SBATCH --array=1-$NUM_JOBS
-# #SBATCH --job-name=$JOB_NAME
-# #SBATCH --output=$LOG_FILE
-# #SBATCH --error=$LOG_FILE
+#SBATCH -n 1
+#SBATCH --time=$TIME
+#SBATCH --mem-per-cpu=6144
+#SBATCH --array=1-$NUM_JOBS
+#SBATCH --job-name=$JOB_NAME
+#SBATCH --output=$LOG_FILE
+#SBATCH --error=$LOG_FILE
 
-# # Environment
-# module load python/$PYVERSION gdal/$GDALVERSION
-# pip install gdal=`gdal-config --version` --user # do not put it in requirements, the version must match local install!
-# pip install -r requirements.txt --user
-
-# python /cluster/work/igp_psr/elwalt/pdm/rs-uncertainty/src/scripts/create_dataset.py --preprocess --cfg $CONFIG_FILES[$SLURM_ARRAY_TASK_ID]
+python /cluster/work/igp_psr/elwalt/pdm/rs-uncertainty/src/scripts/create_dataset.py --preprocess --cfg $CONFIG_FILES[$SLURM_ARRAY_TASK_ID]
