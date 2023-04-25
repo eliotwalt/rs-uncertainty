e3dd80075761b735e15cc86d3bafd0e8d1c400e4
ewalt <eliot.walt@epfl.ch>
2023-04-25 15:45:35 +0200

    add output pipe

--- run/create_cloud_threshold.sh
+++ run/create_cloud_threshold.sh
@@ -1,5 +1,5 @@
-./run/create_dataset.sh --euler --num_projects_per_job 1 --cfg ./config/create_dataset/cloud_threshold_0-01.yaml
-./run/create_dataset.sh --euler --num_projects_per_job 1 --cfg ./config/create_dataset/cloud_threshold_0-25.yaml
-./run/create_dataset.sh --euler --num_projects_per_job 1 --cfg ./config/create_dataset/cloud_threshold_0-50.yaml
-./run/create_dataset.sh --euler --num_projects_per_job 1 --cfg ./config/create_dataset/cloud_threshold_0-75.yaml
-./run/create_dataset.sh --euler --num_projects_per_job 1 --cfg ./config/create_dataset/cloud_threshold_1-00.yaml
+./run/create_dataset.sh --euler --num_projects_per_job 1 --cfg ./config/create_dataset/cloud_threshold_0-01.yaml > ../../logs/dataset/CT_0-01.yaml
+./run/create_dataset.sh --euler --num_projects_per_job 1 --cfg ./config/create_dataset/cloud_threshold_0-25.yaml > ../../logs/dataset/CT_0-25.yaml
+./run/create_dataset.sh --euler --num_projects_per_job 1 --cfg ./config/create_dataset/cloud_threshold_0-50.yaml > ../../logs/dataset/CT_0-50.yaml
+./run/create_dataset.sh --euler --num_projects_per_job 1 --cfg ./config/create_dataset/cloud_threshold_0-75.yaml > ../../logs/dataset/CT_0-75.yaml
+./run/create_dataset.sh --euler --num_projects_per_job 1 --cfg ./config/create_dataset/cloud_threshold_1-00.yaml > ../../logs/dataset/CT_1-00.yaml
