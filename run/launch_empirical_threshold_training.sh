fb156ea5535173c2feb5500766cc794b0f4bc1f5
ewalt <eliot.walt@epfl.ch>
2023-07-28 17:03:18 +0200

    transfer

--- run/launch_empirical_threshold_training.sh
+++ run/launch_empirical_threshold_training.sh
@@ -1,3 +1,7 @@
-./run/launch_training.sh ./config/train/empirical_cloud_threshold_exp_10.yaml
-./run/launch_training.sh ./config/train/empirical_cloud_threshold_exp_30.yaml
-./run/launch_training.sh ./config/train/empirical_cloud_threshold_exp_70.yaml
+M=$1
+for i in $(seq 1 $M);
+do
+    ./run/launch_training.sh ./config/train/empirical_cloud_threshold_exp_10.yaml
+    ./run/launch_training.sh ./config/train/empirical_cloud_threshold_exp_30.yaml
+    ./run/launch_training.sh ./config/train/empirical_cloud_threshold_exp_70.yaml
+done
