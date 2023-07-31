148b0e88c2ff101c337ac40c98571a541c6ccf97
ewalt <eliot.walt@epfl.ch>
2023-07-31 11:12:26 +0200

    transfer

--- run/launch_empirical_threshold_training.sh
+++ run/launch_empirical_threshold_training.sh
@@ -1,7 +1,7 @@
 M=$1
 for i in $(seq 1 $M);
 do
-    ./run/launch_training.sh ./config/train/empirical_cloud_threshold_exp_10.yaml
-    ./run/launch_training.sh ./config/train/empirical_cloud_threshold_exp_30.yaml
-    ./run/launch_training.sh ./config/train/empirical_cloud_threshold_exp_70.yaml
+    ./run/launch_training.sh ./config/train/empirical_cloud_threshold_exp_10.yaml $i
+    ./run/launch_training.sh ./config/train/empirical_cloud_threshold_exp_30.yaml $i
+    ./run/launch_training.sh ./config/train/empirical_cloud_threshold_exp_70.yaml $i
 done
--- run/launch_training.sh
+++ run/launch_training.sh
@@ -1,13 +1,14 @@
 CONFIG_FILE=$1
+PROCESS_INDEX=$2
 
 # parse log name
 log_name=$(echo $(basename $CONFIG_FILE) | cut -d "." -f 1)
-log_f=/cluster/work/igp_psr/elwalt/logs/train/$log_name.log
+log_f=/cluster/work/igp_psr/elwalt/logs/train/{$log_name}_{$PROCESS_INDEX}.log
 mkdir -p $(dirname $log_f)
 touch log_f
 
 # define slurm options
-options="-n 1 --mem-per-cpu=22500 --time=20:00:00 --job-name=train --output=$log_f --error=$log_f --gpus=1 --gres=gpumem:10g" # time: h/epoch ~ 0.2875h, safety: 0.4h
+options="-n 1 --mem-per-cpu=23000 --time=20:00:00 --job-name=train --output=$log_f --error=$log_f --gpus=1 --gres=gpumem:10g" # time: h/epoch ~ 0.2875h, safety: 0.4h, RAM max ~21.76Gb => 23Gb
 
 # lauch
 echo "Submitting training job"
