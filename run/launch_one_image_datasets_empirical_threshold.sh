1b5c497c5af794a3bd65435f2baba8f8cac03e22
ewalt <eliot.walt@epfl.ch>
2023-08-02 11:27:42 +0200

    gee pipeline shell empirical threshold exp

---
+++ run/launch_one_image_datasets_empirical_threshold.sh
@@ -0,0 +1,12 @@
+./run/launch_one_image_datasets_experiment.sh --euler \
+    config/create_dataset/empirical_cloud_threshold_exp_10_gee_template.yaml \
+    config/predict_testset/empirical_cloud_threshold_exp_10_gee_template.yaml \
+    config/evaluate_testset/empirical_cloud_threshold_exp_10_gee_template.yaml
+    ./run/launch_one_image_datasets_experiment.sh --euler \
+    config/create_dataset/empirical_cloud_threshold_exp_30_gee_template.yaml \
+    config/predict_testset/empirical_cloud_threshold_exp_30_gee_template.yaml \
+    config/evaluate_testset/empirical_cloud_threshold_exp_30_gee_template.yaml
+./run/launch_one_image_datasets_experiment.sh --euler \
+    config/create_dataset/empirical_cloud_threshold_exp_70_gee_template.yaml \
+    config/predict_testset/empirical_cloud_threshold_exp_70_gee_template.yaml \
+    config/evaluate_testset/empirical_cloud_threshold_exp_70_gee_template.yaml
