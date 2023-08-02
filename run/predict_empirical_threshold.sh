553d6023a729da675d357230e036ee0726bad546
ewalt <eliot.walt@epfl.ch>
2023-08-02 09:23:28 +0200

    transfer prediction files

---
+++ run/predict_empirical_threshold.sh
@@ -0,0 +1,3 @@
+./run/predict_testset.sh ./config/predict_testset/empirical_cloud_threshold_exp_10.yaml
+./run/predict_testset.sh ./config/predict_testset/empirical_cloud_threshold_exp_30.yaml
+./run/predict_testset.sh ./config/predict_testset/empirical_cloud_threshold_exp_70.yaml
