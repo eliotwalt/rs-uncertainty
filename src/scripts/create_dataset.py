12640b9c439a716f5f14f44af042c6da313c2e7f
ewalt <eliot.walt@epfl.ch>
2023-08-02 12:26:14 +0200

    transfer

--- src/scripts/create_dataset.py
+++ src/scripts/create_dataset.py
@@ -288,7 +288,7 @@ class ProjectsPreprocessor:
         s1_images_ascending = []
         s1_images_descending = []
         # read s2
-        print(f"[debug:291] s2 reprojected dir: {self.run['s2_reprojected_dir']} (exists={os.path.exists(self.run['s2_reprojected_dir'])})")
+        print(f"[debug:291] s2 reprojected dir: {os.path.abspath(self.run['s2_reprojected_dir'])} (exists={os.path.exists(self.run['s2_reprojected_dir'])})")
         for img_path in pjoin(self.run['s2_reprojected_dir'], project_id).glob('*.tif'):
             print(f"[debug:293] img_path: {img_path} (exists={os.path.exists(img_path)})")
             with rasterio.open(img_path) as fh:
